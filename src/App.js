02192c95f133a981d846635b5b3c8dd0f7e184b4
Brian Carter <brian@briancarter.dev>
2020-05-24 11:08:14 -0600

    Prepared for deployment on beta server and for production on AWS S3, with environment variables

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          API Domain: {process.env.REACT_APP_API_DOMAIN}
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
